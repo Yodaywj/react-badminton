65ee6f9f6409640f394e80a77b351eb557a4bc9d
Neo <437238751@qq.com>
2023-04-01 15:50:14 +0100

    变更

--- src/services/logoutService.js
+++ src/services/logoutService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const logoutService = (url) => {
-    axios.delete(url,{withCredentials:true})
+export const logoutService = async (url) => {
+    await axios.delete(url,{withCredentials:true})
         .then(response => {
             console.log(response)
         })
